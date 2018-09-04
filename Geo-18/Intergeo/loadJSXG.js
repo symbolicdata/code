409ffafd0e8122aa7fea56a3e2e3279a676e2325
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2018-09-04 17:24:12 +0200

    Geo-18 moved to feature branch

--- Geo-18/Intergeo/loadJSXG.js
+++
@@ -1,9 +0,0 @@
-function loadJSXG(path)
-{
-    var selection = document.getElementById("selection");
-    path = "file:///home/graebe/git/SD/code/Geo-18/Intergeo/";
-    fname = path + selection.options[selection.selectedIndex].value.toString();
-
-    $('debug').innerHTML = fname+'<br>';
-    board = JXG.JSXGraph.loadBoardFromFile('jxgbox', fname , 'Intergeo');
-}
